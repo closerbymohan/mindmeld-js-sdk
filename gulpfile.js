dadccd49b7e769adff715c6cab8c179622956416
swaraj <swaraj@expectlabs.com>
2014-05-21 16:17:09 -0700

    archive task now re-written in gulp, woo

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,21 @@
 var gulp = require('gulp');
-require('gulp-grunt')(gulp);
+require('gulp-grunt')(gulp); // Load Grunt tasks for jsdoc until gulp-jsdoc becomes more legit
 var clean = require('gulp-clean');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var zip = require('gulp-zip');
+var es = require('event-stream');
+var replace = require('gulp-replace');
+var fs = require('fs');
 
-//BUILD
-//    'clean:dist',
-//    'jsdoc:dist',
-//    'uglify:dist',
-//    'zip:dist',
-//    'buildSearchWidget'
+var baseDirOption = {base: './'};
+var archiveDirectory = './archive/';
+
+// Bower version used to version files
+var bowerVersion = '';
+var versionedMindMeldName = '';
+var versionedMinifiedMindMeldName = '';
 
 gulp.task('cleanBuilt', function () {
    return gulp.src([
@@ -31,6 +35,47 @@ gulp.task('cleanZip', function () {
         .pipe(clean());
 });
 
+// Parses bower.json for current version and sets file names
+// for mindmeld-<version>.js and mindmeld-<version>.min.js
+gulp.task('setVersion', function () {
+    var bowerPath = './bower.json';
+    var bowerData = JSON.parse(fs.readFileSync(bowerPath, 'utf-8'));
+    bowerVersion = bowerData.version;
+    versionedMindMeldName = 'mindmeld-' + bowerVersion + '.js';
+    versionedMinifiedMindMeldName = 'mindmeld-' + bowerVersion + '.min.js';
+});
+
+// Copy mindmeld.js and mindmeld.min.js to archive/ directory
+gulp.task('archiveJS', ['setVersion', 'compressMM'], function () {
+    gulp.src('mindmeld.js')
+        .pipe(rename(versionedMindMeldName))
+        .pipe(gulp.dest(archiveDirectory));
+
+    gulp.src('mindmeld.min.js')
+        .pipe(rename(versionedMinifiedMindMeldName))
+        .pipe(gulp.dest(archiveDirectory));
+});
+
+// Creates archive of SDK at current version
+gulp.task('archiveSDK', ['setVersion', 'archiveJS', 'build'], function () {
+    es.merge(
+        gulp.src([
+            'LICENSE',
+            'docs/**'
+        ], baseDirOption),
+
+        gulp.src([
+            archiveDirectory + versionedMindMeldName,
+            archiveDirectory + versionedMinifiedMindMeldName
+        ], {base: archiveDirectory}),
+
+        gulp.src('HelloWorld.html')
+            .pipe(replace(/mindmeld\.js/, versionedMindMeldName))
+    )
+        .pipe(zip(archiveDirectory + 'mindmeld-js-sdk-' + bowerVersion + '.zip'))
+        .pipe(gulp.dest('./'));
+});
+
 gulp.task('compressMM', ['cleanCompressed'], function () {
    return gulp.src('mindmeld.js')
        .pipe(uglify(), {mangle:true})
@@ -45,7 +90,7 @@ gulp.task('zipSDK', ['grunt-docs', 'compressMM', 'cleanZip'], function () {
        'mindmeld.js',
        'mindmeld.min.js',
        'HelloWorld.html'
-   ], {base: './'})
+   ], baseDirOption)
        .pipe(zip('mindmeld-js-sdk.zip'))
        .pipe(gulp.dest('./'));
 });
