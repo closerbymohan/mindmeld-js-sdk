314afd96d2af059ff2318f058ce098ce5ac3f51a
swaraj <swaraj@expectlabs.com>
2014-05-25 23:27:14 -0700

    moved search widget gulp tasks into separate file

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,6 @@
 var gulp = require('gulp');
 require('gulp-grunt')(gulp); // Load Grunt tasks for jsdoc until gulp-jsdoc becomes more legit
 var chug = require('gulp-chug');
-var uglify = require('gulp-uglify');
-var sass = require('gulp-sass');
-var minifyCSS = require('gulp-minify-css');
-var concat = require('gulp-concat-util');
-var rename = require('gulp-rename');
-var zip = require('gulp-zip');
-var es = require('event-stream');
-var replace = require('gulp-replace');
-var symlink = require('gulp-symlink');
-var fs = require('fs');
 var taskListing = require('gulp-task-listing');
 
 // Voice navigator dependencies
@@ -45,59 +35,11 @@ gulp.task('uglifyMM', function () {
 
 
 // ------------------------ Search Widget Tasks ------------------------ //
-var srcSearchWidgetDir = 'src/widgets/searchWidget/';
-var distSearchWidgetDir = 'dist/widgets/searchWidget/';
+var searchWidgetGulpfilePath = './tasks/searchWidget.js';
 
-gulp.task('buildSearchWidget', ['searchWidgetCSSMin', 'searchWidgetJS']);
-
-// Generates standalone search widget into dist/widgets/searchWidget
-gulp.task('searchWidgetJS', ['uglifyMM', 'uglifySearchWidget'], function () {
-    return gulp.src([
-            srcSearchWidgetDir + 'js/util/vendorHeader.js',
-            srcSearchWidgetDir + 'js/vendor/jquery.min.js',
-            srcSearchWidgetDir + 'js/vendor/jquery-ui-autocomplete.min.js',
-            srcSearchWidgetDir + 'js/util/vendorFooter.js',
-            srcSearchWidgetDir + 'js/util/mmHeader.js',
-            distMMDirectory + 'mindmeld.min.js',
-            distSearchWidgetDir + 'jquery.mindmeld-searchwidget.min.js',
-            srcSearchWidgetDir + 'js/util/mmFooter.js'
-    ])
-        .pipe(concat('mindmeldSearchWidget.js'))
-        .pipe(gulp.dest(distSearchWidgetDir));
-});
-
-// Uglifies search widget
-gulp.task('uglifySearchWidget', function () {
-    return gulp.src(srcSearchWidgetDir + 'js/jquery.mindmeld-searchwidget.js')
-        .pipe(uglify(), {mangle: true})
-        .pipe(rename('jquery.mindmeld-searchwidget.min.js'))
-        .pipe(gulp.dest(distSearchWidgetDir))
-});
-
-// Compiles search widget's SASS into CSS
-gulp.task('searchWidgetSass', function () {
-    return gulp.src(srcSearchWidgetDir + 'sass/main.scss')
-        .pipe(sass())
-        .pipe(rename('mindmeldSearchWidget.css'))
-        .pipe(gulp.dest(distSearchWidgetDir));
-});
-
-// Minifies search widget's CSS
-gulp.task('searchWidgetCSSMin', ['searchWidgetSass'], function () {
-    return gulp.src(distSearchWidgetDir + 'mindmeldSearchWidget.css')
-        .pipe(minifyCSS())
-        .pipe(rename('mindmeldSearchWidget.min.css'))
-        .pipe(gulp.dest(distSearchWidgetDir));
-});
-
-// Watches search widget SASS files
-gulp.task('watchSearchWidgetStyle', ['searchWidgetCSSMin'], function () {
-   gulp.watch(srcSearchWidgetDir + 'sass/**', ['searchWidgetCSSMin']);
-});
-
-// Watches search widget JS files
-gulp.task('watchSearchWidgetJS', ['searchWidgetJS'], function () {
-   gulp.watch(srcSearchWidgetDir + 'js/**', ['searchWidgetJS']);
+gulp.task('buildSearchWidget', function () {
+    return gulp.src(searchWidgetGulpfilePath, {read: false})
+        .pipe(chug());
 });
 
 // --------------------------------------------------------------------- //
