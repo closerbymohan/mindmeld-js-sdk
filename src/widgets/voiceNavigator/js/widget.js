2120598693d4c30744094f75a389d7d00596a7fa
J.J. Jackson <jj@expectlabs.com>
2014-06-19 18:35:33 -0700

    Address VN documentation feedback

--- src/widgets/voiceNavigator/js/widget.js
+++ src/widgets/voiceNavigator/js/widget.js
@@ -11,27 +11,39 @@ var MM = window.MM = window.MM || {};
  *                                                 or '_blank' to open links in a new tab or window. See the target attribute
  *                                                 of [anchor](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a)
  *                                                 elements for more information.
- * @property {String} [listeningMode="normal"]     defines the listening mode of the voice navigator when it _init opened. Acceptable
+ * @property {String} [listeningMode="normal"]     defines the listening mode of the voice navigator when it is opened. Acceptable
  *                                                 values include 'normal', 'continuous', and false. False prevents listening
  *                                                 and the default is 'normal'.
  * @property {Number} [numResults]                 if specified, this number of cards will appear as results
- * @property {String} [cardLayout="default"]       specifies the predefined card layout used. Valid values are 'default', and 'custom'.
- * @property {CardField[]} [cardFields]            an array of card fields {@link CardField}.
- * @property {String} [cardTemplate]               an underscore (lodash) html template which is used to create card representation
- *                                                 of documents. If a value is given, cardLayout will be 'custom'
- * @property {boolean} [resetCardsCSS]             if true, removes CSS specific to the cards container.
- * @property {String} [customCSS]                  specifies custom CSS to be applied to the voice navigator. The CSS will
- *                                                 be included as an embedded style, which takes precedence over external styles.
+ * @property {CardField[]} [cardFields]            an array of card fields which will be appended to the card. With card fields,
+ *                                                 you can render document fields that are specific to your application.
+ *                                                 See {@link CardField} for more information
+ * @property {String} [cardTemplate]               an [underscore](http://underscorejs.org/#template) (or lodash) html
+ *                                                 template which is used to create a card representation of a document
+ *                                                 object. The resulting html, is wrapped in an anchor element which links
+ *                                                 to the document's url. The template is supplied with the document object
+ *                                                 returned by the API. A card template can be used to render any document
+ *                                                 fields that are specific to your application with custom logic.
+ * @property {boolean} [resetCardsCSS]             if true, removes CSS specific to the cards container. This can be helpful
+ *                                                 if the default cards CSS is conflicting with your own customCSS
+ * @property {String} [customCSS]                  specifies custom CSS to be applied to the voice navigator. You can use
+ *                                                 custom CSS to change the appearance of the voice navigator widget and
+ *                                                 it's document cards, to better suit your branding. When using this parameter,
+ *                                                 the styling will be included as embedded CSS, which takes precedence
+ *                                                 over external CSS.
  * @property {String} [customCSSURL]               specifies the url of a file containing custom CSS to be applied to the
- *                                                 voice navigator. It will be applied as an external style.
- *
+ *                                                 voice navigator. This parameter works the same as customCSS, except that
+ *                                                 the styling will be applied as external CSS, by linking to the url provided.
+ *                                                 This can be helpful if you would like to refer to images with relative paths.
  * @property {Number} [baseZIndex=100000]          the voice navigator elements will have a Z index between the value
- *                                                 given and 1000 greater than the value
+ *                                                 given and 1000 greater than the value. If the voice navigator is hidden
+ *                                                 underneath elements on a page, try setting it to something higher.
  *
  */
 
 /**
- * An Object representing a field to display in a document card for the Voice Navigator widget.
+ * An Object representing a field to display in a document card for the Voice Navigator widget. You can use card fields to
+ * quickly include more information on your cards.
  *
  * @typedef {Object} CardField
  * @property {String} key           the key containing the value of this field in document objects. This field must be specified.
@@ -39,11 +51,23 @@ var MM = window.MM = window.MM || {};
  *                                  if omitted the value will be hidden from the card
  * @property {String} [label]       if specified, a label with the provided text will precede the value
  * @property {String} [format]      for formatter to be used to present the value in a user friendly form. Valid formatters
- *                                  are default, and date.
+ *                                  are default, and date. The date format converts unix timestamps into the 'MM/dd/YYYY'
+ *                                  format.
  *
- * @example
- // Will result in the following:
- // Released 10/13/1996
+ * @example <caption> Basic example </caption>
+ *
+ // When author is John Doe -> 'Written By: John Doe'
+ // When author is omitted the field is not displayed
+ //
+ var authorField = {
+   key: 'author',
+   label: 'Written By:',
+ };
+ *
+ * @example <caption> Using the date format </caption>
+ *
+ // When pubdate is Oct. 10, 1996 -> 'Released 10/13/1996'
+ // When pubdate is omitted -> 'Released Unknown'
  //
  var dateField = {
    key: 'pubdate',
@@ -72,25 +96,24 @@ var MM = window.MM = window.MM || {};
  * @example <caption> Loading the voice navigator </caption>
  *
  <script type="text/js">
- // This should be below all other
  var MM = window.MM || {};
- ( function () {
-     MM.loader = {
-         rootURL: 'https://developer.expectlabs.com/public/sdks/',
-         widgets: ['voice']
-     };
-     MM.widgets = {
-         config: {
-             appID: 'YOUR APPID',
-             voice: voiceNavigatorConfig
-         }
-     };
-     var script = document.createElement('script');
-     script.type = 'text/javascript'; script.async = true;
-     script.src = MM.loader.rootURL + 'embed.js';
-     var t = document.getElementsByTagName('script')[0];
-     t.parentNode.insertBefore(script, t);
- }());
+     ( function () {
+         MM.loader = {
+             rootURL: 'https://developer.expectlabs.com/public/sdks/'
+         ,   widgets: ['voice']
+         };
+         MM.widgets = {
+             config: {
+                 appID: 'YOUR APPID'
+             ,   voice: voiceNavigatorConfig  // this object contains your configuration options
+             }
+         };
+         var script = document.createElement('script');
+         script.type = 'text/javascript'; script.async = true;
+         script.src = MM.loader.rootURL + 'embed.js';
+         var t = document.getElementsByTagName('script')[0];
+         t.parentNode.insertBefore(script, t);
+     }());
  </script>
  *
  * @example <caption> Card Template </caption>
@@ -193,6 +216,118 @@ var MM = window.MM = window.MM || {};
      // Now load the voice navigator
  </script>
  *
+ * @example <caption> Advanced example: card template, custom css, and other options </caption>
+ *
+ <script id="card-template" type="text/template">
+     <h2 class="title"><%= title %></h2>
+     <% if (typeof image !== 'undefined' && image.url && image.url !== '') { %>
+         <p class="image not-loaded">
+             <img src="<%= image.url %>">
+         </p>
+     <% } %>
+
+     <%  var desc = "No description";
+         if (typeof description === 'string') {
+             desc = description.substr(0, 150) + (description.length > 150 ? "&hellip;" : "");
+         } %>
+     <p class="description"><%= desc %></p>
+
+     <div class="mm-vn-row">
+     <%  if (typeof rating !== 'undefined' && rating && rating !== '') { %>
+         <p class="align-left rating">
+             <span class="rating-stars stars69x13">
+                 <%  var processedRating = Math.floor(rating * 2 + 0.5) / 2;
+                     var ratingClass = 'r' + processedRating.toString().replace('.', '-');; %>
+                 <span class="rating-stars-grad <%= ratingClass %>"></span>
+                 <span class="rating-stars-img"></span>
+             </span>
+         </p>
+     <%  } else { %>
+         <p class="align-left rating placeholder">No rating</p>
+     <%  }
+         if (typeof reviewcount !== 'undefined' && reviewcount && reviewcount !== '') { %>
+             <p class="align-right review-count">
+             <%  var scales = ['', 'k', 'M'];
+                 var scale = scales.shift();
+                 var value = parseInt(reviewcount);
+                 while (value > 1000 && scales.length > 0) {
+                     scale = scales.shift(); // remove next scale
+                     value = value / 1000;
+                 } %>
+             <%= Math.floor(value * 100) / 100 + scale %> reviews
+             </p>
+     <%  } else { %>
+             <p class="align-right review-count placeholder">No reviews</p>
+     <%  } %>
+     <p class="clear-fix"></p>
+     </div>
+ </script>
+ <script id="vn-card-css" type="text/css">
+     #cards a.card .mm-vn-row p { margin: 2px 0; display: block; }
+     #cards a.card .mm-vn-row p.clear-fix { clear: both; }
+     #cards a.card .mm-vn-row p.align-left { float: left; text-align: left; }
+     #cards a.card .mm-vn-row p.align-right { float: right; text-align: right; }
+     #cards a.card .mm-vn-row p.placeholder { font-size: 10px; font-style: italic; color: #aaa; }
+     #cards a.card .mm-vn-row .rating { display: inline-block; }
+     #cards a.card .mm-vn-row .rating-stars { margin-top: 0; margin-left: 0; position: relative; }
+     #cards a.card .mm-vn-row .rating-stars.stars69x13 { width: 69px; height: 13px; }
+     #cards a.card .mm-vn-row .rating-stars-grad {
+         background: #d77835;
+         background: -moz-linear-gradient(top,#d77835 0,#f08727 40%,#f4a066 100%);
+         background: -webkit-gradient(linear,left top,left bottom,color-stop(0%,#d77835),color-stop(40%,#f08727),color-stop(100%,#f4a066));
+         background: -webkit-linear-gradient(top,#d77835 0,#f08727 40%,#f4a066 100%);
+         filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#d77835',endColorstr='#f4a066',GradientType=0);
+         position: absolute;
+         top: 0;
+         left: 0;
+         height: 13px;
+     }
+     #cards a.card .mm-vn-row .rating-stars-grad.r5   { width: 69px; }
+     #cards a.card .mm-vn-row .rating-stars-grad.r4-5 { width: 63px; }
+     #cards a.card .mm-vn-row .rating-stars-grad.r4   { width: 55px; }
+     #cards a.card .mm-vn-row .rating-stars-grad.r3-5 { width: 49px; }
+     #cards a.card .mm-vn-row .rating-stars-grad.r-3  { width: 41px; }
+     #cards a.card .mm-vn-row .rating-stars-grad.r2-5 { width: 35px; }
+     #cards a.card .mm-vn-row .rating-stars-grad.r2   { width: 27px; }
+     #cards a.card .mm-vn-row .rating-stars-grad.r1-5 { width: 21px; }
+     #cards a.card .mm-vn-row .rating-stars-grad.r1   { width: 14px; }
+     #cards a.card .mm-vn-row .rating-stars-grad.r0-5 { width:  7px; }
+     #cards a.card .mm-vn-row .rating-stars-grad.r0   { width:  0px; }
+     #cards a.card .mm-vn-row .stars69x13 .rating-stars-img {
+        width: 69px;
+        background: url(/public/images/stars.png) no-repeat center center;
+        height: 13px;
+        position: absolute;
+        top: 0;
+        left: 0;
+     }
+ </script>
+ <script type="text/js">
+     var MM = window.MM || {};
+     ( function () {
+         MM.loader = {
+             rootURL: 'https://developer.expectlabs.com/public/sdks/'
+         ,   widgets: ['voice']
+         };
+         MM.widgets = {
+             config: {
+                 appID: 'YOUR APPID'
+             ,   voice: {
+                     cardTemplate: window['vn-card-template'].innerHTML
+                 ,   customCSS: window['vn-custom-css'].innerHTML
+                 ,   listeningMode: 'continuous' // extended listening when opened
+                 ,   cardLinkBehavior: '_blank' // links open in new tabs
+                 }
+             }
+         };
+         var script = document.createElement('script');
+         script.type = 'text/javascript'; script.async = true;
+         script.src = MM.loader.rootURL + 'embed.js';
+         var t = document.getElementsByTagName('script')[0];
+         t.parentNode.insertBefore(script, t);
+     }());
+ </script>
+
  * @memberOf MM
  * @namespace
  */
@@ -290,8 +425,8 @@ function postMessage(action, data) {
  * Opens the voice navigator modal window
  * @param {Object} [options]
  * @param {String} [options.query]                 if provided, this field will be the initial query, and will immediately show results
- * @param {boolean} [options.forceNewIFrame=false] if true, any voice navigators that previously created will be destroyed,
- *                                                 and a new instance will be created.
+ * @param {boolean} [options.forceNewIFrame=false] if true, any voice navigators that have previously been created will
+ *                                                 be destroyed, and a new instance will be created.
  */
 MM.voiceNavigator.showModal = function(options) {
     options = options || {};
